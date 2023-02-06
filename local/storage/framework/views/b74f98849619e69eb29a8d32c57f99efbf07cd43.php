312344570f87ceebfee087696ae6e16a3525de56
nz-hridoy <nayeemuzaman05@gmail.com>
2023-02-06 22:51:02 +0600

    terms of use page

---
+++ local/storage/framework/views/b74f98849619e69eb29a8d32c57f99efbf07cd43.php
@@ -0,0 +1,193 @@
+<?php if (isset($component)) { $__componentOriginalc3251b308c33b100480ddc8862d4f9c79f6df015 = $component; } ?>
+<?php $component = $__env->getContainer()->make(App\View\Components\GuestLayout::class, []); ?>
+<?php $component->withName('guest-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes([]); ?>
+    <?php $__env->startSection('title'); ?>
+        Cashei Terms of Use
+    <?php $__env->stopSection(); ?>
+
+    <style>
+        * {
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            font-family: Arial, sans-serif;
+        }
+        body{
+            padding: 0;
+            margin: 0;
+            -webkit-text-size-adjust: 100%;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-rendering: optimizeLegibility;
+            -webkit-font-feature-settings: "pnum";
+            font-feature-settings: "pnum";
+            font-variant-numeric: proportional-nums;
+            position: relative;
+            background: #eee;
+        }
+        .container {
+            padding-right: 0;
+            padding-left: 0;
+            margin-right: auto;
+            margin-left: auto;
+            margin-top: 7px;
+            margin-bottom: 7px;
+
+        }
+
+        @media (min-width: 768px) {
+            .container {
+                width: 738px;
+                padding-right: 15px;
+                padding-left: 15px;
+                margin-right: auto;
+                margin-left: auto;
+            }
+        }
+
+        @media (min-width: 992px) {
+            .container {
+                width: 962px;
+            }
+        }
+
+        @media (min-width: 1200px) {
+            .container {
+                width: 1170px;
+            }
+        }
+
+        .main-container {
+            color: #222;
+            text-align: left !important;
+            padding: 2em;
+            background: #fff;
+            min-height: 400px;
+            -webkit-box-shadow: 0 0 3px 0 rgba(0, 0, 0, .25);
+            -moz-box-shadow: 0 0 3px 0 rgba(0, 0, 0, .25);
+            box-shadow: 0 0 3px 0 rgba(0, 0, 0, .25)
+        }
+
+        .main-container * {
+            text-align: left !important
+        }
+
+        .main-container :not(a) {
+            color: #222
+        }
+
+        .main-container #content p {
+            margin-top: 1rem;
+            margin-bottom: 1rem
+        }
+
+        a {
+            color: #009c9c
+        }
+
+        a:hover {
+            color: #007bff
+        }
+
+        h2 {
+            font-size: 1.5em
+        }
+
+        @media  only screen and (max-width: 600px) {
+            h2 {
+                font-size: 1.2em
+            }
+        }
+
+    </style>
+
+<div style="background:#eee;">
+    <div class="container">
+        <div class="main-container">
+            <h2 style="margin-top:0;">Cashei Terms of Use</h2>
+            <hr style="margin-bottom: 1em;">
+            <div id="content">
+                <p> By downloading or using the app, these terms will automatically apply to you – you should make
+                    sure therefore that you read them carefully before using the app. You’re not allowed to copy or
+                    modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt
+                    to extract the source code of the app, and you also shouldn’t try to translate the app into
+                    other languages or make derivative versions. The app itself, and all the trademarks, copyright,
+                    database rights, and other intellectual property rights related to it, still belong to Hassan
+                    Hussein. </p>
+                <p> Hassan Hussein is committed to ensuring that the app is as useful and efficient as possible. For
+                    that reason, we reserve the right to make changes to the app or to charge for its services, at
+                    any time and for any reason. We will never charge you for the app or its services without making
+                    it very clear to you exactly what you’re paying for. </p>
+                <p> The Cashei app stores and processes personal data that you have provided to us, to provide my
+                    Service. It’s your responsibility to keep your phone and access to the app secure. We therefore
+                    recommend that you do not jailbreak or root your phone, which is the process of removing
+                    software restrictions and limitations imposed by the official operating system of your device.
+                    It could make your phone vulnerable to malware/viruses/malicious programs, compromise your
+                    phone’s security features and it could mean that the Cashei app won’t work properly or at all.
+                </p>
+                <div>
+                    <p> The app does use third-party services that declare their Terms and Conditions. </p>
+                    <p> Link to Terms and Conditions of third-party service providers used by the app </p>
+                    <ul>
+                        <li><a href="https://policies.google.com/terms" target="_blank"
+                                rel="noopener noreferrer">Google Play Services</a></li>
+                        <!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!----><!---->
+                    </ul>
+                </div>
+                <p> You should be aware that there are certain things that Hassan Hussein will not take
+                    responsibility for. Certain functions of the app will require the app to have an active internet
+                    connection. The connection can be Wi-Fi or provided by your mobile network provider, but Hassan
+                    Hussein cannot take responsibility for the app not working at full functionality if you don’t
+                    have access to Wi-Fi, and you don’t have any of your data allowance left. </p>
+                <p></p>
+                <p> If you’re using the app outside of an area with Wi-Fi, you should remember that the terms of the
+                    agreement with your mobile network provider will still apply. As a result, you may be charged by
+                    your mobile provider for the cost of data for the duration of the connection while accessing the
+                    app, or other third-party charges. In using the app, you’re accepting responsibility for any
+                    such charges, including roaming data charges if you use the app outside of your home territory
+                    (i.e. region or country) without turning off data roaming. If you are not the bill payer for the
+                    device on which you’re using the app, please be aware that we assume that you have received
+                    permission from the bill payer for using the app. </p>
+                <p> Along the same lines, Hassan Hussein cannot always take responsibility for the way you use the
+                    app i.e. You need to make sure that your device stays charged – if it runs out of battery and
+                    you can’t turn it on to avail the Service, Hassan Hussein cannot accept responsibility. </p>
+                <p> With respect to Hassan Hussein’s responsibility for your use of the app, when you’re using the
+                    app, it’s important to bear in mind that although we endeavor to ensure that it is updated and
+                    correct at all times, we do rely on third parties to provide information to us so that we can
+                    make it available to you. Hassan Hussein accepts no liability for any loss, direct or indirect,
+                    you experience as a result of relying wholly on this functionality of the app. </p>
+                <p> At some point, we may wish to update the app. The app is currently available on Android &amp;
+                    iOS – the requirements for the both systems(and for any additional systems we decide to extend
+                    the availability of the app to) may change, and you’ll need to download the updates if you want
+                    to keep using the app. Hassan Hussein does not promise that it will always update the app so
+                    that it is relevant to you and/or works with the Android &amp; iOS version that you have
+                    installed on your device. However, you promise to always accept updates to the application when
+                    offered to you, We may also wish to stop providing the app, and may terminate use of it at any
+                    time without giving notice of termination to you. Unless we tell you otherwise, upon any
+                    termination, (a) the rights and licenses granted to you in these terms will end; (b) you must
+                    stop using the app, and (if needed) delete it from your device. </p>
+                <p><strong>Changes to This Terms and Conditions</strong></p>
+                <p> I may update our Terms and Conditions from time to time. Thus, you are advised to review this
+                    page periodically for any changes. I will notify you of any changes by posting the new Terms and
+                    Conditions on this page. </p>
+                <p> These terms and conditions are effective as of 2023-02-2 </p>
+                <p><strong>Contact Us</strong></p>
+                <p> If you have any questions or suggestions about my Terms and Conditions, do not hesitate to
+                    contact me at contact@cashei.app. </p>
+            </div>
+        </div>
+
+
+
+    </div>
+</div>
+
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc3251b308c33b100480ddc8862d4f9c79f6df015)): ?>
+<?php $component = $__componentOriginalc3251b308c33b100480ddc8862d4f9c79f6df015; ?>
+<?php unset($__componentOriginalc3251b308c33b100480ddc8862d4f9c79f6df015); ?>
+<?php endif; ?>
+<?php /**PATH C:\laragon\www\relincs\local\resources\views/pages/terms_conditions.blade.php ENDPATH**/ ?>
